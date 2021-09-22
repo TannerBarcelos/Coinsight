edc831cac58e95185e6379854988074a55fad70f
Tanner Barcelos <tanner.m.barcelos@gmail.com>
2021-09-21 23:21:02 -0700

    Adding documentation to context

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -5,21 +5,21 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from 'firebase/auth';
+
+// Create a new context instance
 const AuthContext = createContext();
 
-export const useAuth = () => useContext(AuthContext);
-
+// Create our Provider - this is the function which will contain functions and variables we can use globally in the app
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState();
-  const [isAuth, setIsAuth] = useState(false);
 
   const signUp = async (email, password) =>
-    createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPassword(auth, email, password); //firebase func for auth with email and pass
 
   const signIn = (email, password) =>
-    signInWithEmailAndPassword(auth, email, password);
+    signInWithEmailAndPassword(auth, email, password); //firebase func for auth with email and pass
 
-  const logOut = () => signOut(auth);
+  const logOut = () => signOut(auth); //firebase func for auth to logout
 
   // listen for auth changes from the server (Firebase)
   useEffect(() => {
@@ -36,3 +36,7 @@ export const AuthProvider = ({ children }) => {
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
+
+// Export our context as a hook to use anywhere to hook into our data
+export const useAuth = () => useContext(AuthContext);
+// Can see all auth docs on firebase website - their docs are immaculate
